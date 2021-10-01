f3d21d5e5942dc084edde5ff9426a3c7fdd59af9
pseudosma <mrdzpaige@ghoulmail.net>
2021-09-30 21:47:46 -0500

    v2 updates

--- src/__tests__/searchability.test.ts
+++ src/__tests__/searchability.test.ts
@@ -2,34 +2,52 @@ import { Search, Searchable, SearchType } from "../index";
 
 describe("when using search", () => {
   it("should return false if there's no match in endsWith", () => {
-    const s: Searchable = { string: "bar", searchType: SearchType.endsWith };
+    const s: Searchable = {
+      searchTerm: "bar",
+      searchType: SearchType.EndsWith
+    };
     expect(Search(s, "foo")).toBeFalsy();
   });
   it("should return false if there's no match in endsWith", () => {
-    const s: Searchable = { string: "bar", searchType: SearchType.endsWith };
+    const s: Searchable = {
+      searchTerm: "bar",
+      searchType: SearchType.EndsWith
+    };
     expect(Search(s, "foo bar")).toBeTruthy();
   });
   it("should return false if there's no match in includes", () => {
-    const s: Searchable = { string: "bar", searchType: SearchType.includes };
+    const s: Searchable = {
+      searchTerm: "bar",
+      searchType: SearchType.Includes
+    };
     expect(Search(s, "foo")).toBeFalsy();
   });
   it("should return false if there's no match in includes", () => {
-    const s: Searchable = { string: "bar", searchType: SearchType.includes };
+    const s: Searchable = {
+      searchTerm: "bar",
+      searchType: SearchType.Includes
+    };
     expect(Search(s, "foo bar far")).toBeTruthy();
   });
   it("should return false if there's no match in startsWith", () => {
-    const s: Searchable = { string: "bar", searchType: SearchType.startsWith };
+    const s: Searchable = {
+      searchTerm: "bar",
+      searchType: SearchType.StartsWith
+    };
     expect(Search(s, "foo")).toBeFalsy();
   });
   it("should return false if there's no match in startsWith", () => {
-    const s: Searchable = { string: "bar", searchType: SearchType.startsWith };
+    const s: Searchable = {
+      searchTerm: "bar",
+      searchType: SearchType.StartsWith
+    };
     expect(Search(s, "bar foo")).toBeTruthy();
   });
   it("should do strict equality check if the searchType is invalid", () => {
-    expect(Search({ string: "foo", searchType: 4 }, "bar")).toBeFalsy();
+    expect(Search({ searchTerm: "foo", searchType: 4 }, "bar")).toBeFalsy();
   });
   it("should fail strict equality check if the searchType is invalid", () => {
-    expect(Search({ string: "foo", searchType: 4 }, "foo")).toBeTruthy();
+    expect(Search({ searchTerm: "foo", searchType: 4 }, "foo")).toBeTruthy();
   });
   it("should throw an error if a searchable is not passed in", () => {
     expect(Search(undefined, "")).toBeFalsy();
